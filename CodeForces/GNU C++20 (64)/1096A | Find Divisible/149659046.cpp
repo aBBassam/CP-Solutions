65eea16a4aad7f855026fcd0bbde5c4d90f3a376
Muhammed alajam <75698815+MuhammedAlajam@users.noreply.github.com>
2022-08-19 15:38:20 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1096A | Find Divisible/149659046.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define answer(x) cout << (x ? "YES\n" : "NO\n")
+#define test ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); int T; cin >> T; while(T--)
+#define go ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define F first
+#define S second
+using namespace std;
+ 
+int main()
+{
+  test
+  {
+   ll l, r; cin >> l >> r; 
+   cout << l << ' ' << l * 2 << '\n';
+  }  
+}
+ 
+ 
+ 
