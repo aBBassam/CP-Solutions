0e135fa0f8ab623f6f8acb8514aff9564a3d0ee8
Muhammed alajam <75698815+MuhammedAlajam@users.noreply.github.com>
2022-08-19 15:33:32 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1663B | Mike's Sequence/152376590.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define answer(x) cout << (x ? "YES\n" : "NO\n")
+#define test ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); int T; cin >> T; while(T--)
+#define go ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define F first
+#define S second
+using namespace std;
+ 
+ 
+ 
+ 
+int main()
+{
+ int n; cin >> n; 
+ if(n < 1200) cout << 1200; 
+ else if(n < 1400) cout << 1400; 
+ else if(n < 1600) cout << 1600; 
+ else if(n < 1900) cout << 1900; 
+ else if(n < 2100) cout << 2100; 
+ else if(n < 2300) cout << 2300; 
+ else if(n < 2400) cout << 2400; 
+ else if(n < 2600) cout << 2600; 
+ else if(n < 3000)  cout << 3000; 
+}
+ 
+ 
+ 
