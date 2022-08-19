ed9b822993e377e4ce7e83d519c13a161a6aacca
Muhammed alajam <75698815+MuhammedAlajam@users.noreply.github.com>
2022-08-19 15:17:23 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1694A | Creep/160952746.cpp
@@ -0,0 +1,55 @@
+// Problem: A. Creep
+// Contest: Codeforces - Codeforces Round #800 (Div. 2)
+// URL: https://codeforces.com/contest/1694/problem/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+#include <bits/stdc++.h>
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define answer(x) cout << (x ? "YES\n" : "NO\n")
+#define test ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); int T; cin >> T; while(T--)
+#define go ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define F first
+#define S second
+using namespace std;
+ 
+ 
+int a, b; 
+ 
+ 
+void solve( )
+{
+ cin >> a >> b; 
+ 
+ int ind = a > b; 
+ 
+ while(a || b)
+ {
+  if(a && !b) cout << 0, a--; 
+  else if(b && !a) cout << 1, b--; 
+  else 
+  {
+   if(ind&1) cout << 0, a--; 
+   else cout << 1, b--;
+  }
+  
+  
+  ind ++;
+ }
+  
+  
+ cout << '\n';
+}
+ 
+ 
+int main()
+{
+    test
+  solve();
+}
+ 
+ 
+ 
