2c3d74f1bdd7db6684da3042f341648de3be0ce6
Muhammed alajam <75698815+MuhammedAlajam@users.noreply.github.com>
2022-08-19 15:33:45 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1663A | Who Tested?/152339362.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define answer(x) cout << (x ? "YES\n" : "NO\n")
+#define test ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); int T; cin >> T; while(T--)
+#define go ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define F first
+#define S second
+using namespace std;
+ 
+ 
+ 
+ 
+int main()
+{
+ cout << "BucketPotato";
+}
+ 
+ 
+ 
