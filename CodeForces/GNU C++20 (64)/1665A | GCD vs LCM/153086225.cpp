0ff9270af9cf1e2ef6c28d74c122792cec7e117d
Muhammed alajam <75698815+MuhammedAlajam@users.noreply.github.com>
2022-08-19 15:32:18 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1665A | GCD vs LCM/153086225.cpp
@@ -0,0 +1,35 @@
+// Problem: A. GCD vs LCM
+// Contest: Codeforces - Codeforces Round #781 (Div. 2)
+// URL: https://codeforces.com/contest/1665/problem/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+ 
+#include <bits/stdc++.h>
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define answer(x) cout << (x ? "YES\n" : "NO\n")
+#define test ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); int T; cin >> T; while(T--)
+#define go ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define F first
+#define S second
+using namespace std;
+ 
+ 
+ 
+ 
+ 
+void solve( )
+{
+ ll n; 
+ cin >> n ;
+ cout << 1 << ' ' << n - 3 << ' ' << 1 << ' ' << 1 << '\n';
+}
+ 
+ 
+int main()
+{
+    test
+  solve();
+}
