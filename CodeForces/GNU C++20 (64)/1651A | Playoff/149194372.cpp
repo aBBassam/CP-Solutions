a422c15ed6585f5f6691dce1e4351e6ca6c72ec7
Muhammed alajam <75698815+MuhammedAlajam@users.noreply.github.com>
2022-08-19 15:40:08 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1651A | Playoff/149194372.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define answer(x) cout << (x ? "YES\n" : "NO\n")
+#define test ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); int T; cin >> T; while(T--)
+#define go ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define F first
+#define S second
+using namespace std;
+ 
+ 
+ 
+ 
+int main()
+{
+ test
+ {
+  ll n; cin >> n ;
+  
+  ll ans = 1; 
+  
+  while(n -- )
+   ans *= 2; 
+   
+  cout << ans -1 << '\n';
+ }
+}
+ 
+ 
+ 
